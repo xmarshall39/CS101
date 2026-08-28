dabce878dde94a5006c3408dc0d0c71a5db7b92e
Xavier Marshall <48485277+xmarshall39@users.noreply.github.com>
2026-08-27 21:07:25 -0400

    frak

---
+++ Lesson 7/Students/midnightzamboni/frank.py
@@ -0,0 +1,38 @@
+import re
+
+file = open("frankenstein.txt", "r", encoding="utf-8")
+
+wordCounter= {}
+
+
+s = "\\\\Hello\\r"
+
+while True:
+    frankLine = file.readline()
+    lineWords = frankLine.split()
+    for word in lineWords:
+        strippedWord = word.strip(".,-'\"_ ( ) ;?!")
+        match = re.match("^[a-zA-Z0-9_]*$", word)
+        if not match:
+            continue
+        strippedWord = match.group().lower()
+        if strippedWord not in wordCounter:
+            wordCounter[strippedWord] = 1
+        elif strippedWord in wordCounter:
+            wordCounter[strippedWord] += 1
+
+    if not frankLine:
+        break
+
+userWord = input("Find your word (type \\quit to quit):\n").lower()
+
+while userWord != "\\quit":
+    if userWord in wordCounter:
+        print(f"{userWord} is in 'Frankenstein' {wordCounter[userWord]} times.")
+        userWord = input("Find your word (type \\quit to quit):\n").lower()
+    elif userWord == "\\quit":
+        print("Goodbye.")
+        file.close()
+    else:
+        print("Try again!")
+        userWord = input("Find your word (type \\quit to quit):\n").lower()
